1ba8629b0c0f41bb3e236e76dfe677ed07df36d4
Jop <j.bertrams@home.nl>
2022-10-06 11:25:52 +0200

    Added Vue Router

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      <router-link to="/">Home</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>
+      <router-link to="/about">About</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
