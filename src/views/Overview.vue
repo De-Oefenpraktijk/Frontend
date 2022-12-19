7776e71054ae641a25b838ad78574c3c651ed72e
Rense <rensevanstiphout@gmail.com>
2022-12-19 10:01:02 +0100

    pish

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -35,27 +35,10 @@
         <span>Workspaces</span>
       </div>
       <div class="mainCard__body">
-        <div class="card">
+
+        <div class="card" v-for="(workspace) in workspaces.slice(0,2)" :key="workspace.id">
           <img
-            src="https://via.placeholder.com/200x300?text=Couple+And+Family"
-            alt="profile picture"
-          />
-        </div>
-        <div class="card">
-          <img
-            src="https://via.placeholder.com/200x300?text=Sleep"
-            alt="profile picture"
-          />
-        </div>
-        <div class="card">
-          <img
-            src="https://via.placeholder.com/200x300?text=Forensic"
-            alt="profile picture"
-          />
-        </div>
-        <div class="card">
-          <img
-            src="https://via.placeholder.com/200x300?text=Gero"
+            src="https://via.placeholder.com/200x300?text=" + {{index}}.{{workspace.name}}
             alt="profile picture"
           />
         </div>
@@ -188,7 +171,12 @@ export default {
   },
   setup(){
     const { user } = useAuth0();
+     const token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Imp2M0F6YndkMzFmWmFldzJOcW1GVSJ9.eyJpc3MiOiJodHRwczovL29lZmVucHJha3RpamsuZXUuYXV0aDAuY29tLyIsInN1YiI6IlNab0NFWkJvMDVCc21DWW1TZmMwYTR0Q3VCZXZzTkM0QGNsaWVudHMiLCJhdWQiOiJodHRwczovL2FwaS5vZWZlbnByYWt0aWprLm5sIiwiaWF0IjoxNjcxMTAxNjgyLCJleHAiOjE2NzExODgwODIsImF6cCI6IlNab0NFWkJvMDVCc21DWW1TZmMwYTR0Q3VCZXZzTkM0Iiwic2NvcGUiOiJyZWFkOnVzZXIiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMiLCJwZXJtaXNzaW9ucyI6WyJyZWFkOnVzZXIiXX0.0_rZRzHP9DMC-6gfzPO130EkdenLLjXocXui4pECcVLIITobi5GHF5N2C0Ug5-_FUC7T2Qq-GSZ5cELAlMhg-QMsWRu9g1CX9gnfNIvoqSqg4VPu_wZ9BWMQ7NtdgxJyp6mxdToVerR_0q7A1KSYXwodwQEkEqMmh3aTA9Wh8pKPhVvZCe9iTW-sGd7LIUnxaA7zojZk-2yGPAzNr5a-8RU40zOVDa5yJLsyz1PSdD2f2RITbkaOEX_DWSkLFvwdqqZBx1Eihh4rFxwK9n0jYYiuxCz7M_B3NHMIyxnsU8sbRdjaJ6vvwE4I2smYVYwncuDLEKqOMpZeXjmWgimRUQ";
+      const config = {
+            headers: { Authorization: `Bearer ${token}` }};
+    const workspaces = axios.get('http://20.126.206.207/workspace/getworkspaces', config)
     return {
+      workspaces,
       user
     };
   },
