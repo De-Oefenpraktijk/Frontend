d3d3d9ecd6c65a02501e9e7e4497c0c1f9cbcc3c
Rayco Haex <raycoh1299@gmail.com>
2023-01-17 12:32:52 +0100

    script for api data on social

--- src/views/Social.vue
+++ src/views/Social.vue
@@ -1,20 +1,40 @@
 <template>
   <Sidebar />
   <div class="main">
-    <Topbar title="Messages" />
+    <Topbar title="Social" />
   </div>
 </template>
+
+
 
 <script>
 import Sidebar from '../components/Sidebar.vue';
 import Topbar from '../components/Topbar.vue';
 
 export default {
-  name: 'Messages',
+  name: 'Social',
   components: {
     Topbar,
     Sidebar,
   },
+  data() {
+    return {
+      openRequests: [],
+      friends: [],
+    };
+  },
+  methods: {
+    async getWorkspaceData() {
+      const config = {
+        headers: { Authorization: `Bearer ${store.token}` },
+      };
+      var test = await axios.get(
+        'http://20.126.206.207/getRequest/'
+      ).then((response) => {
+        this.openRequests = response.data.openRequests;
+      });
+    },
+  }
 };
 </script>
 
