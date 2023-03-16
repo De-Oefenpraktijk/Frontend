9c3c16207965dd33f4b74e983f222015d9ef83cb
SStoychev01 <stoycho.stoychev@tmc.nl>
2023-03-16 15:47:06 +0100

    Define sceleton for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
-import './App.css';
-import './components/SideBar/SideBar'
-import React from 'react';
-import SideBar from './components/SideBar/SideBar';
-import { Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import Profile from './pages/Profile/Profile';
-import WorkspacesOverview from './pages/WorkspacesOverview/WorkspacesOverview';
-import TopBar from './components/TopBar/TopBar';
-import AppLayout from './Layout/AppLayout';
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home/HomePage";
+import ProfilePage from "./pages/Profile/ProfilePage";
+import WorkspacesPage from "./pages/Workspaces/WorkspacesPage";
+import AppLayout from "./Layout/AppLayout";
+import Blank from "./pages/NoContentPage/Blank";
+import Workspace from "./components/Workspaces/Workspace";
 
 function App() {
   return (
-    <div>
-
-
-      <Routes>
-        <Route path='/' element={<AppLayout></AppLayout>} >
-          <Route path='/profile' element={<Profile />}></Route>
-          <Route path='/' element={<HomePage />}></Route>
-          <Route path='/workspaces' element={<WorkspacesOverview />} ></Route>
-
-        </Route>
-      </Routes>
-
-      <SideBar></SideBar>
-
-    </div>
+    <Routes>
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/overview" element={<Blank />} />
+        <Route path="/search" element={<Blank />} />
+        <Route path="/workspaces" element={<WorkspacesPage />} />
+        <Route path="/social" element={<Blank />} />
+        <Route path="/calendar" element={<Blank />} />
+        <Route path="/articles" element={<Blank />} />
+        <Route path="/forum" element={<Blank />} />
+        <Route path="/settings" element={<Blank />} />
+        <Route path="/organization" element={<Blank />} />
+        <Route path="/workspaces" element={<WorkspacesPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        {/* Testing */}
+        <Route path="/workspace" element={<Workspace />} />
+      </Route>
+    </Routes>
   );
 }
 
