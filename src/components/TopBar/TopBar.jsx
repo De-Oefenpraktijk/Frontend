13efdb8b065e1d335c990d676e8e3691b5245d38
SemWenders <i447923@fhict.nl>
2023-03-20 13:40:26 +0100

    display username

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,43 +1,45 @@
-import React from "react"
-import './TopBar.css'
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import "./TopBar.css";
 
 export default function TopBar() {
-    return (
-        <div className="topbar">
-            <div className="title">
-                <h1>Title</h1>
+  const { user } = useAuth0();
+
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <div className="topbar">
+      <div className="title">
+        <h1>Title</h1>
+      </div>
+      <div>
+        <div className="topbar__profile">
+          <div className="topbar__profile__dropdown">
+            <div className="topbar__profile__dropdown__button">
+              <span>{user.given_name}</span>
+              <font-awesome-icon icon="fa-solid fa-chevron-down" />
             </div>
-            <div>
-                <div className="topbar__profile">
-                    <div className="topbar__profile__dropdown">
-                        <div className="topbar__profile__dropdown__button">
-                            <span>username</span>
-                            <font-awesome-icon icon="fa-solid fa-chevron-down" />
-                        </div>
-                        <div className="topbar__profile__dropdown__content">
-                            <ul>
-
-                                <li>
-                                    <font-awesome-icon icon="fa-solid fa-user" />
-                                    <span>Profile</span>
-                                </li>
-                                <li>
-                                    <font-awesome-icon
-                                        icon="fa-solid fa-arrow-right-from-bracket"
-                                    />
-                                    <span>Logout</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <img
-                        src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
-                        alt="profile picture"
-                    />
-                </div>
-            </div >
-        </div >
-
-    )
+            <div className="topbar__profile__dropdown__content">
+              <ul>
+                <li>
+                  <font-awesome-icon icon="fa-solid fa-user" />
+                  <span>Profile</span>
+                </li>
+                <li>
+                  <font-awesome-icon icon="fa-solid fa-arrow-right-from-bracket" />
+                  <span>Logout</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <img
+            src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+            alt="profile picture"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
-
