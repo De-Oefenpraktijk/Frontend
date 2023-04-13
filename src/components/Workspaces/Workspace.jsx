9a91505ee97411eec86f8e545add36391ab00d5d
Stelian Rumenov <stelian@duodeka.nl>
2023-04-13 13:54:16 +0200

    working with legit data

--- src/components/Workspaces/Workspace.jsx
+++ src/components/Workspaces/Workspace.jsx
@@ -10,7 +10,10 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Button from "@mui/material/Button";
 import Moment from "moment-timezone";
 import { formatDistanceToNowStrict } from "date-fns";
-import { GETUSERROOMSBYWORKSPACEURL } from "../../service/ConnectionStrings";
+import {
+  GETUSERROOMSBYWORKSPACEURL,
+  GETPUBLICROOM,
+} from "../../service/ConnectionStrings";
 
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
@@ -27,7 +30,6 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import { padding } from "@mui/system";
 
 export default function Workspace() {
   const { workspaceId } = useParams();
@@ -37,6 +39,7 @@ export default function Workspace() {
   const [open, setOpen] = useState(false); //Used for the Modal
   const [meetingRooms, setMeetingRooms] = useState([]);
   const [workspaceName, setWorkspaceName] = useState([]);
+  const [publicRooms, setPublicRooms] = useState([]);
   const [refreshRooms, setRefreshRooms] = useState(false);
 
   //Params
@@ -58,13 +61,14 @@ export default function Workspace() {
     return result;
   };
   const triggerRefreshRoom = () => setRefreshRooms(!refreshRooms);
+
   const joinRoom = (roomId) => {
     navigate(`/workspace/join-room/${roomId}`);
   };
 
   //Effects
   useEffect(() => {
-    //Set rooms and workspace name
+    //Set rooms, public rooms and workspace name
     axios
       .get(GETUSERROOMSBYWORKSPACEURL + workspaceId + "/" + userId)
       .then((response) => {
@@ -74,23 +78,16 @@ export default function Workspace() {
       .catch((err) => {
         console.log("error: " + err);
       });
-  }, [refreshRooms]);
 
-  const temporaryPublicRooms = [
-    {
-      roomId: "6433da370c3028ad5cc90061",
-      date: new Date("December 17, 1995 03:24:00"),
-      name: "Name 1",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      roomId: "2",
-      name: "Name 2",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-  ];
+    axios
+      .get(`http://localhost:5137/api/v1/PublicRoom/${workspaceId}`)
+      .then((response) => {
+        setPublicRooms(response.data);
+      })
+      .catch((err) => {
+        console.log("error: " + err);
+      });
+  }, [refreshRooms]);
 
   return (
     <div id="workspace-info">
@@ -137,10 +134,7 @@ export default function Workspace() {
                     {handleDateDifference(room["scheduledDate"])}
                   </TableCell>
                   <TableCell align="center">
-                    <Button
-                      variant="outlined"
-                      onClick={() => joinRoom(room.roomId)}
-                    >
+                    <Button variant="outlined" onClick={() => joinRoom()}>
                       Join meeting
                     </Button>
                   </TableCell>
@@ -168,43 +162,57 @@ export default function Workspace() {
           <Form.Label>Available Webinars</Form.Label>
 
           <Paper elevation={3}>
-            <Box padding={1}>
-              {temporaryPublicRooms.map((room) => (
-                <>
-                  <Card sx={{ maxWidth: "100%" }}>
-                    <CardContent>
-                      {/* <Link to={`/workspace/${workspace["id"]}`}> */}
-                      <Typography gutterBottom variant="h5" component="div">
-                        {room.name}
-                      </Typography>
-                      <Typography variant="body2" color="text.secondary">
-                        {room.description}
-                      </Typography>
-                      <Typography
-                        variant="body2"
-                        color="text.secondary"
-                        sx={{ fontWeight: "bold", m: 0 }}
-                      >
-                        Webinar starts in: {handleDateDifference(room.date)}
-                      </Typography>
-                      <CardActions sx={{ float: "right", paddingRight: 0 }}>
-                        {/* <Button variant="outlined" size="large">
-                          Share
-                        </Button> */}
-                        <Button
-                          variant="outlined"
-                          size="large"
-                          onClick={() => joinRoom(room.roomId)}
+            <Box
+              padding={0}
+              sx={{
+                display: "flex",
+                flexWrap: "wrap",
+                alignSelf: "flex-end",
+                "& > :not(style)": {
+                  m: 1,
+                  width: "100%",
+                  height: "100%",
+                },
+              }}
+            >
+              <Form.Label>Available Webinars</Form.Label>
+
+              <Box padding={1}>
+                {publicRooms.map((publicRoom) => (
+                  <>
+                    <Card sx={{ maxWidth: "100%" }}>
+                      <CardContent>
+                        <Typography gutterBottom variant="h4" component="div">
+                          {console.log(publicRoom)}
+
+                          {publicRoom.roomName}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          {publicRoom.description}
+                        </Typography>
+                        <Typography
+                          variant="body2"
+                          color="text.secondary"
+                          sx={{ fontWeight: "bold", m: 0 }}
                         >
-                          Join
-                        </Button>
-                      </CardActions>
-                      {/* </Link> */}
-                    </CardContent>
-                  </Card>
-                  <br></br>
-                </>
-              ))}
+                          Webinar starts in:{" "}
+                          {handleDateDifference(publicRoom.date)}
+                        </Typography>
+                        <CardActions sx={{ float: "right", paddingRight: 0 }}>
+                          <Button
+                            variant="outlined"
+                            size="large"
+                            onClick={() => joinRoom(publicRoom.roomId)}
+                          >
+                            Join
+                          </Button>
+                        </CardActions>
+                      </CardContent>
+                    </Card>
+                    <br></br>
+                  </>
+                ))}
+              </Box>
             </Box>
           </Paper>
         </Box>
--- src/service/ConnectionStrings.js
+++ src/service/ConnectionStrings.js
@@ -2,6 +2,8 @@ export const URL = "http://localhost:5137/api/v1/";
 
 export const ROOMURL = "http://20.86.203.49/api/v1/";
 
+export const GETPUBLICROOM = URL + "PublicRoom/"
+
 export const GETWORKSPACEURL = ROOMURL + "Workspace";
 
 export const GETROOMURL = ROOMURL + "Room/";
