a93eef5d2c476a0cd89100098182c38e9bfe2e19
Valeri <t.videv@student.fontys.nl>
2023-05-07 05:45:56 +0200

    working on cypress

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  video: true,
+  videosFolder: "cypress/videos",
+  screenshotsFolder: "cypress/screenshots",
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 5000,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
