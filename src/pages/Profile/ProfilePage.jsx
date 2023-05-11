88eee454e11206a7552cccdb7366fd3b19e36430
Stoycho Stoychev <stoycho.stoychev@tmc.nl>
2023-05-11 14:53:56 +0200

    Add profesional user data functionality

--- src/pages/Profile/ProfilePage.jsx
+++ src/pages/Profile/ProfilePage.jsx
@@ -2,9 +2,17 @@ import { Button } from "@mui/material";
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import Chip from "@mui/material/Chip";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import Stack from "@mui/material/Stack";
 import userProfileService from "../../service/userProfileService";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import Select from "@mui/material/Select";
+import getAllFunctions from "../../service/getAllFunctions";
+import getAllEducations from "../../service/getAllEducations";
+import getAllSpecializations from "../../service/getAllSpecializations";
 import "./ProfilePage.css";
 
 const userDataSkeleton = {
@@ -13,36 +21,76 @@ const userDataSkeleton = {
   username: "",
   email: "",
   workplace: "",
+  function: "",
+  educations: [],
+  specializations: [],
+  biography: "",
+  hobbies: [],
 };
 
 export default function ProfilePage() {
   const [userData, setUserData] = useState(userDataSkeleton);
+  const [allFunctions, setAllFunctions] = useState([{id:"",name:""}]);
+  const [allEducations, setAllEducations] = useState([""]);
+  const [allSpecializations, setAllSpecializations] = useState([""]);
+
   const { user } = useAuth0();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setUserData({...userData, [name]: value});
+    setUserData({ ...userData, [name]: value });
   };
 
+  const handleSelectEducationsChange = (event, values) => {
+    setUserData({ ...userData, ["educations"]: values });
+  };
+  const handleSelectSpecializationsChange = (event, values) => {
+    setUserData({ ...userData, ["specializations"]: values });
+  };
+  const handleSelectHobbiesChange = (event, values) => {
+    setUserData({ ...userData, ["hobbies"]: values });
+  };
   const getUserEmail = () => {
-    return process.env.NODE_ENV === "production" ? user.email : "example@gmail.com"
-  }
+    return process.env.NODE_ENV === "production"
+      ? user.email
+      : "example@gmail.com";
+  };
 
   const updateProfileData = async () => {
     try {
-      const response = await userProfileService.updateUserByEmail(getUserEmail(), userData);
-      console.log("Updated user: ", response);
-    } catch(e) {
+      const response = await userProfileService.updateUserByEmail(
+        getUserEmail(),
+        userData
+      );
+    } catch (e) {
       console.log("Problem occured while updating the profile information!");
       alert("Problem occured while updating the profile information!");
     }
-  }
+  };
 
   useEffect(() => {
     const getUserData = async () => {
       const response = await userProfileService.getUserByEmail(getUserEmail());
       setUserData(response);
     };
+
+    const getAllFunctionsData = async () => {
+      const response = await getAllFunctions();
+      setAllFunctions(response);
+    };
+
+    const getAllSpecializationsData = async () => {
+      const response = await getAllSpecializations();
+      setAllSpecializations(response);
+    };
+
+    const getAllEducationsData = async () => {
+      const response = await getAllEducations();
+      setAllEducations(response);
+    };
+    getAllEducationsData();
+    getAllFunctionsData();
+    getAllSpecializationsData();
     getUserData();
   }, []);
 
@@ -60,62 +108,156 @@ export default function ProfilePage() {
             />
           </div>
           <div className="personal_details">
-            <div>
-              <label>First Name</label>
-              <input
-                name="firstName" 
-                className="input-default"
-                type="text"
-                placeholder="First Name"
-                defaultValue={userData.firstName}
-                onChange={handleChange}
-              />
-            </div>
-            <div>
-              <label>Last Name</label>
-              <input
-                name="lastName"
-                className="input-default"
-                type="text"
-                placeholder="Last Name"
-                defaultValue={userData.lastName}
-                onChange={handleChange}
-              />
-            </div>
+            <Stack spacing={3} sx={{ width: 300 }}>
+              <div>
+                <label>First Name</label>
+                <input
+                  name="firstName"
+                  className="input-default"
+                  type="text"
+                  placeholder="First Name"
+                  defaultValue={userData.firstName}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>Last Name</label>
+                <input
+                  name="lastName"
+                  className="input-default"
+                  type="text"
+                  placeholder="Last Name"
+                  defaultValue={userData.lastName}
+                  onChange={handleChange}
+                />
+              </div>
 
-            <div>
-              <label>Username</label>
-              <input
-                name="username"
-                className="input-default"
-                type="text"
-                placeholder="Username"
-                defaultValue={userData.username}
+              <div>
+                <label>Username</label>
+                <input
+                  name="username"
+                  className="input-default"
+                  type="text"
+                  placeholder="Username"
+                  defaultValue={userData.username}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>Email</label>
+                <input
+                  name="email"
+                  className="input-default"
+                  type="text"
+                  placeholder="Email"
+                  defaultValue={userData.email}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                <label>Workplace</label>
+                <input
+                  name="workplace"
+                  className="input-default"
+                  type="text"
+                  placeholder="Workplace"
+                  defaultValue={userData.workplace}
+                  onChange={handleChange}
+                />
+              </div>
+
+              {/* Function */}
+              <Select
+                id="standard-select-currency"
+                name="function"
+                label="Function"
+                value={userData.function}
                 onChange={handleChange}
+                variant="standard"
+              >
+                {allFunctions.map((option) => (
+                  <MenuItem key={option.id} value={option.name}>
+                    {option.name}
+                  </MenuItem>
+                ))}
+              </Select>
+
+              {/* Educations */}
+              <Autocomplete
+                multiple
+                id="tags-standard"
+                name="educations"
+                options={allEducations.map((education) => education.name)}
+                onChange={handleSelectEducationsChange}
+                value={userData.educations}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="standard"
+                    label="Educations"
+                    placeholder="Favorites"
+                  />
+                )}
               />
-            </div>
-            <div>
-              <label>Email</label>
-              <input
-                name="email"
-                className="input-default"
-                type="text"
-                placeholder="Email"
-                defaultValue={userData.email}
+
+              {/* Specializations */}
+              <Autocomplete
+                multiple
+                name="specializations"
+                onChange={handleSelectSpecializationsChange}
+                id="tags-standard"
+                options={allSpecializations.map((specialization)=>specialization.name)}
+                value={userData.specializations} //TODO Change
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="standard"
+                    label="Specializations"
+                    placeholder="Favorites"
+                  />
+                )}
+              />
+
+              {/* Biography */}
+              <TextField
+                id="outlined-multiline-static"
+                label="Biography"
+                name="biography"
                 onChange={handleChange}
+                multiline
+                rows={4}
+                value={userData.biography} //TODO Change
               />
-            </div>
-            <div>
-              <label>Workplace</label>
-              <input
-                name="workplace"
-                className="input-default"
-                type="text"
-                placeholder="Workplace"
-                defaultValue={userData.workplace}
-                onChange={handleChange}
+
+              {/* Hobbies */}
+              <Autocomplete
+                multiple
+                id="tags-filled"
+                value={userData.hobbies}
+                name="hobbies"
+                onChange={handleSelectHobbiesChange}
+                options={[""]}
+                freeSolo
+                renderTags={(value, getTagProps) =>
+                  value.map((option, index) => (
+                    <Chip
+                      variant="outlined"
+                      label={option}
+                      {...getTagProps({ index })}
+                    />
+                  ))
+                }
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="filled"
+                    label="Hobbies"
+                    placeholder="Your hobbies"
+                  />
+                )}
               />
-            </div>
+            </Stack>
+
             <div className="buttons" v-if="isOwnProfile()">
               <Button onClick={updateProfileData}>Update</Button>
             </div>
