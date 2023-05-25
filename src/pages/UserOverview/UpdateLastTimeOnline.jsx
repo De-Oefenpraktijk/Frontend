790cb02b0805e4ebaf1e52157956770db8fd00cb
tymob0 <87370293+tymob0@users.noreply.github.com>
2023-05-25 11:35:14 +0200

    added service for UpdateLastTimeOnline

--- src/pages/UserOverview/UpdateLastTimeOnline.jsx
+++ src/pages/UserOverview/UpdateLastTimeOnline.jsx
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const UpdateLastTimeOnline = () => {
   const TIME_INTERVAL = 60000;
+  const { user } = useAuth0();
+  const userId = user.sub.split("|")[1];
+
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log("Logs every minute");
+      axios
+        .put(`https://localhost:7147/api/v1/User/UpdateActivityStatus/${userId}`)
+        .then(response => {
+          console.log("PUT request sent successfully. User is online.");
+          // Handle response or perform additional actions if needed
+        })
+        .catch(error => {
+          console.error("Error sending PUT request:", error);
+          // Handle error if needed
+        });
     }, TIME_INTERVAL);
 
     return () => clearInterval(interval);
