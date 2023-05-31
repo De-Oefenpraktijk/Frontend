4c21685fd33a004b821fa1cd1258a4bf54296d44
SemWenders <i447923@fhict.nl>
2023-05-31 19:32:24 +0200

    added api call

--- src/pages/UserOverview/UpdateLastTimeOnline.jsx
+++ src/pages/UserOverview/UpdateLastTimeOnline.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const UpdateLastTimeOnline = () => {
-  const TIME_INTERVAL = 60000;
+  const TIME_INTERVAL = 6000;
+  const config = {
+    method: "put",
+    url: "http://localhost:7147/api/v1/user/updateactivitystatus/646f2535cb5634aabf8463a7",
+    headers: {
+      contentType: "application/json",
+    },
+  };
   useEffect(() => {
     const interval = setInterval(() => {
+      axios(config).then((response) => response.data);
       console.log("Logs every minute");
     }, TIME_INTERVAL);
 
