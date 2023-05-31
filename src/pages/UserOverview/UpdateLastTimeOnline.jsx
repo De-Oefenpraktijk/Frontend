348effb6655f5a3c42ed8be246bb3fb9df71bffb
SemWenders <i447923@fhict.nl>
2023-05-31 20:17:33 +0200

    update status on page load

--- src/pages/UserOverview/UpdateLastTimeOnline.jsx
+++ src/pages/UserOverview/UpdateLastTimeOnline.jsx
@@ -7,6 +7,21 @@ const UpdateLastTimeOnline = () => {
   const TIME_INTERVAL = 60000;
   const { user } = useAuth0();
   const email = user.email;
+
+  useEffect(() => {
+    console.log("TEST");
+    axios
+      .put(`${UPDATE_USER_ACTIVITY}${email}`)
+      .then((response) => {
+        console.log("PUT request sent successfully. User is online.");
+        // Handle response or perform additional actions if needed
+      })
+      .catch((error) => {
+        console.error("Error sending PUT request:", error);
+        // Handle error if needed
+      });
+  }, []);
+
   useEffect(() => {
     const interval = setInterval(() => {
       axios
